b05f01d3768780c003b4e356e98e8ef1af7523e9
mhmd-aashik <aashikdevelop@gmail.com>
2024-09-08 18:03:55 +0530

    Environment Setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 200 300 400 500 600 700 900",
+});
+
+export const metadata: Metadata = {
+  title: "Store Admin DashBoard",
+  description: "Store DashBoard for Admin",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
